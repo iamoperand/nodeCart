f581a2c4cd34c7c378d2802a6caff8ddbcae075b
Nikhil Arora <narora200@gmail.com>
2017-06-15 00:15:10 +0530

    Comments added

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const cartApiRoute = require('./routes/api');
 
 app.use('/', cartApiRoute);
 
+app.get('/', (req, res)=>{
+	res.redirect('/public');
+})
+
 app.listen(2222, function(){
 	console.log("Listening on port 2222");
 })
